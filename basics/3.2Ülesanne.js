a5e73ab4b2aa2943f494394b02d7c8645d4c3ebd
Randel Väär <152606802+shkyn@users.noreply.github.com>
2024-09-03 14:57:53 +0300

    Create 3.2Ülesanne.js

---
+++ basics/3.2Ülesanne.js
@@ -0,0 +1,10 @@
+let küsib
+küsib = prompt("Sisestage ringide arv: ")
+console.log("sisestasite ringide arvu " + küsib + " korda.")
+
+
+
+let progand
+if (küsib % 2 == 0) {
+	console.log("porgandite kogu arv on " + küsib * 2)
+} else (console.log("porgandite kogu arv on " + (küsib - 1) * 2))
