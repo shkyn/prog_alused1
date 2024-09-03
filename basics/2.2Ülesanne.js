46fab9107e8bb14401352c03e30cfe4201a05679
Randel Väär <152606802+shkyn@users.noreply.github.com>
2024-09-03 13:27:52 +0300

    Create 2.2Ülesanne.js

---
+++ basics/2.2Ülesanne.js
@@ -0,0 +1,11 @@
+let küsib
+küsib = prompt("Siesetage Leedu perekonna nimi: ")
+
+if (küsib.endsWith("ne")) {
+	console.log("Abielus")
+} else if 
+	(küsib.endsWith("te")) {
+  console.log("Vallaline")
+  } else if (küsib.endsWith("e")) {
+  	console.log("Määramata")
+  }
