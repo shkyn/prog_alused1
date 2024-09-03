102b8bc07a0c92df8a727f3af0cee8cfdb650e99
Randel Väär <152606802+shkyn@users.noreply.github.com>
2024-09-03 14:39:14 +0300

    Create 3.1Ülesanne.js

---
+++ basics/3.1Ülesanne.js
@@ -0,0 +1,7 @@
+let küsib
+küsib = prompt("Sisestage mitu korda äratada: ")
+console.log("tahtsite äratust: " + küsib + " korda.")
+		
+for (let i = 0; i < küsib; i++) {
+	console.log("Tõuse ja sära!")
+}
