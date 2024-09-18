4900769d07b335ded8319c1c7f9ea667bcc4b8db
Randel Väär <152606802+shkyn@users.noreply.github.com>
2024-09-18 15:03:24 +0300

    Delete Ylesanne6-7 directory

--- Ylesanne6-7/script.js
+++
@@ -1,6 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    var yearSpan = document.getElementById('current-year');
-    var currentYear = new Date().getFullYear();
-    yearSpan.textContent = currentYear;
-});
